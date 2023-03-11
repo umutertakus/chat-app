9d64dab487f4adae49f1731f40a103b45010bbf9
umutertakus <umutertakus@hotmail.com>
2023-03-11 20:09:28 +0300

    Create Auth component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import Auth from "./components/Auth";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
+
 function App() {
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+
   return (
     <div className="App">
-      Chat App
+      <Auth />
     </div>
   );
 }
---
+++ src/components/Auth.js
@@ -0,0 +1,25 @@
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
+
+const Auth = () => {
+  const signInWithGoogle = async () => {
+    try {
+      const response = await signInWithPopup(auth, provider);
+      cookies.set("auth-token", response.user.refreshToken);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="auth">
+      <p>Sign In With Google To Continue</p>
+      <button onClick={signInWithGoogle}>Sign In With Google</button>
+    </div>
+  );
+};
+
+export default Auth;
